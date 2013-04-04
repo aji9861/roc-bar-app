ace842da9b7d7cc780666648ee2d26d6636cd54c
Cory <cms5822@rit.edu>
2013-04-04 11:42:06 -0400

    GPS Enabler
    When the application is opened it will check if the GPS is enabled. If
    not, it will prompt the user to do so.

--- RocBarFinder/src/com/agile/rocbarfinder/MainActivity.java
+++ RocBarFinder/src/com/agile/rocbarfinder/MainActivity.java
@@ -2,7 +2,12 @@ package com.agile.rocbarfinder;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        checkGPSEnabled();
     }
 
 
@@ -20,4 +27,31 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private void checkGPSEnabled(){
+    	String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+    	if(!provider.equals("")){
+    		// Gps is enabled
+    		Toast.makeText(MainActivity.this, "GPS Enabled: " + provider,
+    		          Toast.LENGTH_LONG).show();
+    	}
+    	else{
+    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your GPS module is disabled. Would you like to enable it ?")
+                   .setCancelable(false)
+                   .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);     						
+     						startActivity(intent);
+                            dialog.dismiss();
+                       }
+                   })
+                   .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                       }
+                   });
+            AlertDialog alert = builder.create();
+            alert.show();
+    	}    	
+    }
 }
