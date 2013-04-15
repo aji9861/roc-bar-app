94c480fcb06a62c85b062524c53cdd6557e21bd1
bwall <nightstrike9809@gmail.com>
2013-04-15 14:38:57 -0400

    Added database result sorting methods

---
+++ RocBarFinder/src/com/agile/rocbarfinder/BarComparatorDistance.java
@@ -0,0 +1,20 @@
+package com.agile.rocbarfinder;
+
+import java.util.Comparator;
+
+public class BarComparatorDistance implements Comparator<BarInformation> {
+
+	private double lat;
+	private double lon;
+	
+	public BarComparatorDistance(double lat, double lon){
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	@Override
+	public int compare(BarInformation lhs, BarInformation rhs) {
+		return rhs.getDistance(this.lat, this.lon).compareTo(lhs.getDistance(this.lat, this.lon));		
+	}
+
+}
